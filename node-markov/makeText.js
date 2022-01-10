a65292c027f52eaa37c3f8bd3728aa4c2399d69b
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-01-09 18:18:27 -0800

    Added makeText.js script

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -1,2 +1,48 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const markov = require("./markov");
+const process = require("process");
+const axios = require("axios");
 
+function generateText(text) {
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+function makeText(path) {
+    fs.readFile(path, "utf8", function(err, data) {
+        if (err) {
+            console.error(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        } else {
+            generateText(data);
+            console.log(data);
+        }
+    });
+}
+
+async function makeURL(url) {
+    try {
+        let res = await axios.get(url);
+        console.log(res.data);
+    } catch (err) {
+        console.error(`Error fetching ${url}: ${err}`);
+        process.exit(1);
+    }
+    generateText(res.data);
+}
+
+let path;
+let method;
+
+if (process.argv[2] === "method") {
+    method = process.argv[3];
+    path = process.argv[4];
+} else {
+    path = process.argv[2];
+}
+if (path.slice(0, 4) === "http") {
+    makeURL(path, method);
+} else {
+    makeText(path, method);
+}
