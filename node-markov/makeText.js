cf55925b7c222dd23c049b84113924a4c24979d8
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-01-09 18:52:52 -0800

    Added working solution

--- node-markov/makeText.js
+++ node-markov/makeText.js
@@ -5,14 +5,14 @@ const process = require("process");
 const axios = require("axios");
 
 function generateText(text) {
-    let mm = new markov.MarkovMachine(text);
+    let mm = new markov.MarkovMachine();
     console.log(mm.makeText());
 }
 
 function makeText(path) {
     fs.readFile(path, "utf8", function(err, data) {
         if (err) {
-            console.error(`Error reading ${path}: ${err}`);
+            console.error(`Error reading: ${path} ${err}`);
             process.exit(1);
         } else {
             generateText(data);
@@ -32,17 +32,29 @@ async function makeURL(url) {
     generateText(res.data);
 }
 
-let path;
-let method;
+// let path;
+// let method;
 
-if (process.argv[2] === "method") {
-    method = process.argv[3];
-    path = process.argv[4];
+// if (process.argv[2] === "method") {
+//     method = process.argv[3];
+//     path = process.argv[4];
+// } else {
+//     path = process.argv[2];
+// }
+// if (path.slice(0, 4) === "http") {
+//     makeURL(path, method);
+// } else {
+//     makeText(path, method);
+// }
+
+
+let [method, path] = process.argv.slice(2);
+
+if (method === "file") {
+    makeText(path);
+} else if (method === "url") {
+    makeURL(path);
 } else {
-    path = process.argv[2];
-}
-if (path.slice(0, 4) === "http") {
-    makeURL(path, method);
-} else {
-    makeText(path, method);
+    console.error(`Unknown method: ${method}`);
+    process.exit(1);
 }
--- node-markov/markov.js
+++ node-markov/markov.js
@@ -2,12 +2,11 @@
 
 
 class MarkovMachine {
-
     /** build markov machine; read in text.*/
 
     constructor(text) {
         let words = text.split(/[ \r\n]+/);
-        this.words = words.filter(c => c !== "");
+        this.words = words.filter((c) => c !== "");
         this.makeChains();
     }
 
@@ -18,6 +17,7 @@ class MarkovMachine {
 
     makeChains() {
         let chains = new Map();
+
         for (let i = 0; i < this.words.length; i++) {
             let word = this.words[i];
             let nextWord = this.words[i + 1] || null;
@@ -25,28 +25,31 @@ class MarkovMachine {
             if (chains.has(word)) chains.get(word).push(nextWord);
             else chains.set(word, [nextWord]);
         }
+
         this.chains = chains;
     }
+
 
     static choice(ar) {
         return ar[Math.floor(Math.random() * ar.length)];
     }
 
 
-    /** return random text from chains */
-
     makeText(numWords = 100) {
+        // pick a random key to begin
         let keys = Array.from(this.chains.keys());
         let key = MarkovMachine.choice(keys);
         let out = [];
+
         while (out.length < numWords && key !== null) {
             out.push(key);
             key = MarkovMachine.choice(this.chains.get(key));
         }
-        return out.join("");
+
+        return out.join(" ");
     }
 }
 
 module.exports = {
-    MarkovMachine,
+    MarkovMachine
 };
